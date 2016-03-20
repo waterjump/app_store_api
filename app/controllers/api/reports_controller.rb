fd79665154c8d37639e0336f06cab567b7b4edab
Sean Barrett <sbarrett@weblinc.com>
2016-03-20 19:42:07 -0400

    Adds parameter validation on controller.

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -3,8 +3,23 @@ module Api
     respond_to :json
 
     def show
-      @results = { "hello" => "world" }
-      render json: @results
+      if validate_params
+        Rails.logger.info "69BOT - params: #{params}"
+        @results = { 'hello' => 'world' }
+        render json: @results
+      else
+        render :json => {:error => 'Bad Request'}.to_json, :status => 400
+      end
+    end
+
+    private
+
+    def validate_params
+      required_parameters = [:category_id, :monetization]
+      valid = required_parameters.reduce(true) do |memo, param|
+        memo = memo && params.include?(param)
+      end
+      valid
     end
   end
 end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -2,17 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Api::ReportsController, type: :controller do
   describe 'GET #show' do
-    it "responds successfully with an HTTP 200 status code" do
-      get :show, id: '', format: :json
+    let!(:good_params) do
+      {
+        id: '',
+        format: :json,
+        category_id: 1025,
+        monetization: 'paid'
+      }
+    end
+
+    let!(:bad_params) do
+      {
+        id: '',
+        format: :json
+      }
+    end
+
+    it 'responds successfully with an HTTP 200 status code' do
+      get :show, good_params
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it 'returns a simple response' do
-      get :show, id: '', format: :json
+      get :show, good_params
       results = JSON.parse(response.body)
       expect(results['hello']).to eq 'world'
     end
+
+    it 'requires certain parameters' do
+      get :show, bad_params
+      expect(response).to have_http_status(400)
+    end
   end
 end
-
