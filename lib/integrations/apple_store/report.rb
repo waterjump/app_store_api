b72196e1f8159e76285c63429fb33eb64b50f429
Sean Barrett <sbarrett@weblinc.com>
2016-03-27 22:17:07 -0400

    Adjusts code style

--- lib/integrations/apple_store/report.rb
+++ lib/integrations/apple_store/report.rb
@@ -55,11 +55,11 @@ module AppleStore
         'Accept-Encoding' => 'gzip, deflate, sdch',
         'Accept-Language' => 'en-US,en;q=0.8,lv;q=0.6',
         'User-Agent' =>
-          ['iTunes/11.1.1 (Windows; Microsoft Windows 7 x64 Ultimate Edition',
-           'Service Pack 1 (Build 7601)) AppleWebKit/536.30.1'].join(' '),
+          'iTunes/11.1.1 (Windows; Microsoft Windows 7 x64 Ultimate Edition'\
+           ' Service Pack 1 (Build 7601)) AppleWebKit/536.30.1',
         'Accept' =>
-          ['text/html,application/xhtml+xml',
-           'application/xml;q=0.9,image/webp,*/*;q=0.8'].join(','),
+          'text/html,application/xhtml+xml'\
+          ',application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Cache-Control' => 'max-age=0',
         'X-Apple-Store-Front' => '143441-1,17'
       }
